2a75e14f0e167f1ad5ee0fe7a92d6442bf7e4aac
Ryota Enoki <ryoenoki2013@gmail.com>
2022-06-09 12:08:49 +0900

    responsiveness: small screen

--- lib/helpers/responsiveness.dart
+++ lib/helpers/responsiveness.dart
@@ -6,16 +6,22 @@ const int mediumScreenSize = 768;
 const int smallScreenSize = 360;
 
 class Responsiveness extends StatelessWidget {
-  final Widget largeScreenSize;
-  final Widget mediumScreenSize;
-  final Widget smallScreenSize;
+  final Widget largeScreen;
+  final Widget mediumScreen;
+  final Widget smallScreen;
 
   const Responsiveness({
     Key? key,
-    required this.largeScreenSize,
-    required this.mediumScreenSize,
-    required this.smallScreenSize,
+    required this.largeScreen,
+    required this.mediumScreen,
+    required this.smallScreen,
   }) : super(key: key);
+
+  //Boolean to determine user screen size
+
+  // Is it a small screen? (Less than 768)
+  static bool isSmallScreen(BuildContext context) =>
+      MediaQuery.of(context).size.width < mediumScreenSize;
 
   @override
   Widget build(BuildContext context) {}
