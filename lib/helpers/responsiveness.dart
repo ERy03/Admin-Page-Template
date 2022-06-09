e5dd4258597728c5b3d9f871d1ed760dc7876e97
Ryota Enoki <ryoenoki2013@gmail.com>
2022-06-09 11:42:05 +0900

    responsiveness

---
+++ lib/helpers/responsiveness.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Responsiveness extends StatelessWidget {
+  const Responsiveness({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {}
+}
