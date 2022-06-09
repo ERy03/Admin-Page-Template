a8d75665acd4eacba5099ea7c9bb5fc24877175e
Ryota Enoki <ryoenoki2013@gmail.com>
2022-06-09 11:45:28 +0900

    responsiveness: screen sizes

--- lib/helpers/responsiveness.dart
+++ lib/helpers/responsiveness.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+const int largeScreenSize = 1366;
+const int mediumScreenSize = 768;
+const int smallScreenSize = 360;
+
 class Responsiveness extends StatelessWidget {
   const Responsiveness({Key? key}) : super(key: key);
 
