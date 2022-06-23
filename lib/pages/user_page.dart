2b7e5fc6126afb566b4ab4d1a6385188ba2614d5
Ryota Enoki <ryoenoki2013@gmail.com>
2022-06-23 18:30:28 +0900

    alert dialog

--- lib/pages/user_page.dart
+++ lib/pages/user_page.dart
@@ -12,6 +12,26 @@ class UserPage extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Text("Home"),
+          TextButton(
+            onPressed: () => showDialog<String>(
+              context: context,
+              builder: (BuildContext context) => AlertDialog(
+                title: const Text('AlertDialog Title'),
+                content: const Text('AlertDialog description'),
+                actions: <Widget>[
+                  TextButton(
+                    onPressed: () => Navigator.pop(context, 'Cancel'),
+                    child: const Text('Cancel'),
+                  ),
+                  TextButton(
+                    onPressed: () => Navigator.pop(context, 'OK'),
+                    child: const Text('OK'),
+                  ),
+                ],
+              ),
+            ),
+            child: const Text('Show Dialog'),
+          )
         ],
       ),
     );
